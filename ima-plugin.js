b32784b6033aa1c1566d3546289b4e44d251afae
veritme <zamora_bryan@ymail.com>
2019-06-24 19:17:54 -0500

    Add files via upload

---
+++ ima-plugin.js
@@ -0,0 +1,24 @@
+
+import 'https://cdn.jsdelivr.net/gh/mysuf/videojs-ima-player/src/css/videojs.ima.css';
+
+import videojs from 'video.js';
+import 'videojs-contrib-ads';
+import 'https://cdn.jsdelivr.net/gh/mysuf/videojs-ima-player/src/ima-player.js';
+
+// basic plugin is enough for this purpose
+videojs.registerPlugin('ima', function(options) {
+	// inits contrib-ads asap if not initialized yet
+	if (!this.ads) {
+		console.error("ima-player error: contrib-ads must be registered on player.")
+		return;
+	}
+
+	if (typeof this.ads === "function") {
+		this.ads(Object.assign({
+			debug: options.debug||false,
+			timeout: options.timeout||5000
+		}, options.contribAdsSettings || {}));
+	}
+
+	this.ima = this.addChild('imaPlayer', options);
+});
